2be883ff074d27b98b53e21e22c8eda81d59e680
dssahu <74927562+dssahu@users.noreply.github.com>
2021-08-16 15:26:04 +0530

    Update ADAKING.cpp

--- ADAKING.cpp
+++ ADAKING.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long int k,i,j;
+	    cin>>k;
+	    for(i=0;i<8;i++)
+	    {
+	        for(j=0;j<8;j++)
+	        {
+	            if(i==0 && j==0)
+	            {
+	                cout<<'O';
+	                k--;
+	            }
+	            else if (k>0)
+	            {
+	                cout<<'.';
+	                k--;
+	            }
+	            else
+	            cout<<'X';
+	        }
+	        cout<<endl;
+	    }
+	}
+	return 0;
+}
 
